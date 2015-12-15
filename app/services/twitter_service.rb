aec5e865254d89748604702e90bf4ce04cc189b9
Pat Wey <patrickwey@gmail.com>
2015-12-15 14:09:36 -0700

    Start work on TwitterService

---
+++ app/services/twitter_service.rb
@@ -0,0 +1,10 @@
+class TwitterService
+  def initialize(user) # pass in current user
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["twitter_api_key"]
+      config.consumer_secret     = ENV["twitter_secret_key"]
+      config.access_token        = user.oauth_token
+      config.access_token_secret = user.oauth_token_secret
+    end
+  end
+end
